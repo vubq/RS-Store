48c5e1377bcc9f4b952ea3c742c7a972c9164820
vubq <vubq242002@gmail.com>
2024-07-02 10:58:31 +0700

    Init project BE

---
+++ src/main/java/com/vubq/rsstore/RsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.vubq.rsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vubq/rsstore/RsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vubq.rsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
